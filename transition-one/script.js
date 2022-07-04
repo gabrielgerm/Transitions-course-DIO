e7eb8c1ad351a61e5c684b6c42136ffc966b8d5e
gabrielgerm <gabriel-germano333@hotmail.com>
2022-07-03 22:42:02 -0300

    last commit

---
+++ transition-one/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+// function getNumbers() {
+// 	let number1 = Number(prompt('Insira o primeiro numero'))
+// 	let number2 = Number(prompt('Insira o segundo numero'))
+// 	let sum = number1 + number2;
+	
+// 	if (sum > 10) {
+// 		if (sum > 20) {
+// 			number1 == number2 ? alert(`Os números ${number1} e ${number2} são iguais. Sua soma é ${sum}, que é maior que 10 e maior que 20.`) : alert(
+// 				`Os números ${number1} e ${number2} não são iguais. Sua soma é ${sum}, que é maior que 10 e menor que 20.`)
+// 		} else {
+// 			number1 == number2 ? alert(`Os números ${number1} e ${number2} são iguais. Sua soma é ${sum}, que é maior que 10 e menor que 20.`) : alert(
+// 				`Os números ${number1} e ${number2} não são iguais. Sua soma é ${sum}, que é maior que 10 e menor que 20. `)
+// 		}
+// 	} else if (sum < 10) {
+// 		if (sum > 20) {
+// 			number1 == number2 ? alert(`Os números ${number1} e ${number2} são iguais. Sua soma é ${sum}, que é menor que 10 e maior que 20.`) : alert(
+// 				`Os números ${number1} e ${number2} não são iguais. Sua soma é ${sum}, que é menor que 10 e maior que 20.`)
+// 		} else {
+// 			number1 == number2 ? alert(`Os números ${number1} e ${number2} são iguais. Sua soma é ${sum}, que é menor que 10 e menor que 20.`) : alert(
+// 				`Os números ${number1} e ${number2} não são iguais. Sua soma é ${sum}, que é menor que 10 e menor que 20. `)
+// 		}
+// 	}
+// }
+// getNumbers()
